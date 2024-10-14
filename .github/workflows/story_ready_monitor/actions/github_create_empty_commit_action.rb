22c2f03872baa9d8beb880b00710dae42c7caf50
Scott Werner <stwerner@vt.edu>
2024-10-14 14:53:53 -0400

    try using a different method for creating an empty commit

--- .github/workflows/story_ready_monitor/actions/github_create_empty_commit_action.rb
+++ .github/workflows/story_ready_monitor/actions/github_create_empty_commit_action.rb
@@ -1,5 +1,6 @@
 class GithubCreateEmptyCommitAction < Sublayer::Actions::Base
-  def initialize(repo:, branch:, commit_message:)
+  def initialize(repo:, branch:, commit_message: "Empty commit")
+    super(repo: repo)
     @repo = repo
     @branch = branch
     @commit_message = commit_message
@@ -8,21 +9,12 @@ class GithubCreateEmptyCommitAction < Sublayer::Actions::Base
 
   def call
     begin
-      # Get the latest commit on the branch
-      latest_commit = @client.ref(@repo, "heads/#{@branch}")
-      base_tree = @client.commit(@repo, latest_commit.object.sha).commit.tree.sha
-
-      # Create a new tree with no changes
-      new_tree = @client.create_tree(@repo, [], base_tree: base_tree)
-
-      # Create a new commit
-      new_commit = @client.create_commit(@repo, @commit_message, new_tree.sha, latest_commit.object.sha)
-
-      # Update the reference of the branch to point to the new commit
-      @client.update_ref(@repo, "heads/#{@branch}", new_commit.sha)
-
-      Sublayer.configuration.logger.log(:info, "Empty commit created successfully on #{@branch} in #{@repo}")
-      new_commit.sha
+      latest_commit = @client.ref(@repo, "heads/#{@branch}").object.sha
+      @client.create_commit(@repo, @commit_message, latest_commit, latest_commit)
+      ref = @client.update_ref(@repo, "heads/#{@branch}", latest_commit)
+      commit_url = ref.object.url
+      Sublayer.configuration.logger.log(:info, "Empty commit created successfully on branch #{@branch}: #{commit_url}")
+      commit_url
     rescue Octokit::Error => e
       error_message = "Error creating empty commit: #{e.message}"
       Sublayer.configuration.logger.log(:error, error_message)
