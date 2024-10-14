51febf4f958d1a673b6f5b20f86fc518afb96ae8
Scott Werner <stwerner@vt.edu>
2024-10-14 16:46:43 -0400

    Fix key formatting

--- .github/workflows/cucumber_pr_merged_monitor/cucumber_pr_merged_monitor.rb
+++ .github/workflows/cucumber_pr_merged_monitor/cucumber_pr_merged_monitor.rb
@@ -19,7 +19,7 @@ branch_name = GithubGetPRBranchNameAction.new(repo: repo, pr_number: pr_number).
 issue_number = branch_name.split('-').last
 
 pr_info = GetPRChangesAction.new(repo: repo, pr_number: pr_number).call
-issue_info = JiraGetIssueDescriptionAction.new(issue_key: issue_number).call
+issue_info = JiraGetIssueDescriptionAction.new(issue_key: "CRM-#{issue_number}").call
 project_context = GetContextAction.new(path: "#{ENV['GITHUB_WORKSPACE']}/crm").call
 
 implementation_plan = ImplementationPlanGenerator.new(
